6f0388874b24ada409eb128b53d1336b12650b1f
Monika Verma <monikakasanya@gmail.com>
2023-09-04 17:58:49 +0530

    added some association and validation

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,3 +1,7 @@
 class Node < ApplicationRecord
   belongs_to :tree
+  belongs_to :parent, class_name: 'Node', optional: true
+  has_many :children, class_name: 'Node', foreign_key: 'parent_id'
+
+  validates :label, presence: true
 end
--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,2 +1,3 @@
 class Tree < ApplicationRecord
+  has_many :nodes, dependent: :destroy
 end
