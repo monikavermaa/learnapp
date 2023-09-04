ce7f6999a828e5b95dff24d996838b32ee46330f
Monika Verma <monikakasanya@gmail.com>
2023-09-04 18:25:25 +0530

    create tree, other actions perform

--- app/controllers/api/tree_controller.rb
+++ app/controllers/api/tree_controller.rb
@@ -1,7 +1,31 @@
+# app/controllers/api/tree_controller.rb
 class Api::TreeController < ApplicationController
+  before_action :set_tree
+
+  # GET /api/tree
   def index
+    render json: @tree.as_json(include: { nodes: { include: { children: {} } } })
   end
 
+  # POST /api/tree
   def create
+    @node = Node.new(node_params)
+    @node.tree = @tree
+
+    if @node.save
+      render json: { message: 'Node created successfully' }, status: :created
+    else
+      render json: { errors: @node.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_tree
+    @tree = Tree.first_or_create # Assuming there's only one tree
+  end
+
+  def node_params
+    params.require(:node).permit(:label, :parent_id)
   end
 end
